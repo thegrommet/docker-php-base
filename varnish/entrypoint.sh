fc9717a53b2ae2fe9df1ff1e759aefeac35d8dff
Arthur <git@arthur.pro>
2019-09-23 15:56:00 -0400

    HAproxy 2.0.6, Varnish 6.0.4

--- varnish/entrypoint.sh
+++ varnish/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-varnishd -s malloc,${VARNISH_MEMORY} -a :${VARNISH_PORT} -f /etc/varnish/default.vcl -p http_resp_hdr_len=131072 -p feature=+esi_disable_xml_check
+varnishd -s malloc,${VARNISH_MEMORY} \
+  -a :${VARNISH_PORT},PROXY \
+  -f /etc/varnish/default.vcl \
+  -p http_resp_hdr_len=131072 \
+  -p http_resp_size=163840 \
+  -p workspace_backend=163840 \
+  -p feature=+http2
 sleep 1
 varnishlog
