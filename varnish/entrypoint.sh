2feeeaba1e26ab65c3290562958d1e9ccf31ce07
Arthur <git@arthur.pro>
2019-09-23 18:14:24 -0400

    Varnish log is disabled by default

--- varnish/entrypoint.sh
+++ varnish/entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-varnishd -s malloc,${VARNISH_MEMORY} \
+_VARNISH_PARAMS="\
+  -s malloc,${VARNISH_MEMORY} \
   -a :${VARNISH_PORT},PROXY \
   -f /etc/varnish/default.vcl \
   -p http_resp_hdr_len=131072 \
   -p http_resp_size=163840 \
   -p workspace_backend=163840 \
-  -p feature=+http2
-sleep 1
-varnishlog
+  -p feature=+http2 \
+"
+
+if [ $VARNISHLOG_ENABLED != 1 ]; then _VARNISH_PARAMS="-F $_VARNISH_PARAMS"; fi
+
+varnishd $_VARNISH_PARAMS
+
+if [ $VARNISHLOG_ENABLED == 1 ]; then
+  sleep 1
+  varnishlog -x Debug -x Link
+fi
